bee2b8b2df8de798f3ba0bde189176ba06ef0c39
Aileen OBriengit config --global user.name Aileen <aileentobrien@gmail.com>
2016-10-06 01:04:39 +0000

    adding countindeces file

---
+++ week1/week1day3/countindeces.js
@@ -0,0 +1,18 @@
+
+function countletters (input) {
+var noSpaces = input.replace(/ /g,'');
+
+var objletters = {};
+
+for (var i = 0 ; i < noSpaces.length; i++) {
+  objletters[noSpaces[i]] = [];
+
+}
+
+for (var i = 0 ; i < noSpaces.length; i++) {
+ // objletters[noSpaces[i]] = objletters[noSpaces[i]] + 1;
+ objletters[noSpaces[i]].push(i);
+}
+return objletters
+}
+console.log(countletters("lighthouse in the house"));
