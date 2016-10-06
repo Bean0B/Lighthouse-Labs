615dc50bf052b562d21ffb25d010cf0976906917
Aileen OBriengit config --global user.name Aileen <aileentobrien@gmail.com>
2016-10-06 20:59:19 +0000

    find waldo with index

---
+++ week1day4/callbackarguments.js
@@ -0,0 +1,21 @@
+//Modify the callback function so it logs the index of the array where Waldo is found,
+//ie. "Found Waldo at index 3!". (You will need to modify actionWhenFound to receive the index.)
+
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == "Waldo") {
+      found(i);   // execute callback
+
+    }
+  }
+}
+
+function actionWhenFound(i) {
+  console.log("Found Waldo at index: " + i);
+
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
