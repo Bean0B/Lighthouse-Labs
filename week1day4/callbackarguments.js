c3a4dc25865d92a83e789218b9e6fc979194e0d4
Aileen OBriengit config --global user.name Aileen <aileentobrien@gmail.com>
2016-10-06 21:43:14 +0000

    add for loop to callbackwaldo proj

--- week1day4/callbackarguments.js
+++ week1day4/callbackarguments.js
@@ -3,13 +3,16 @@
 
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "Waldo") {
-      found(i);   // execute callback
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] == "Waldo") {
+      arr.forEach(function(entry, index){
+        if ( entry == "Waldo") {
+      found(index);   // execute callback
 
     }
-  }
+  })
 }
+
 
 function actionWhenFound(i) {
   console.log("Found Waldo at index: " + i);
